4a7581a241f56d4374467c584882dce5f08d5862
Brayan-PM <78266710+BrayanPradoMarroquin@users.noreply.github.com>
2022-07-03 00:31:03 -0600

    Prediccion lineal y funciones de tendencia

--- web.py
+++ web.py
@@ -1,10 +1,8 @@
 import streamlit as st
-import pickle
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
 from sklearn import linear_model
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures
 import numpy as np
 from PIL import Image
@@ -30,6 +28,8 @@ def main():
 
     nivel = st.sidebar.text_input('Ingrese en grado del polinomio')
 
+    pred = st.sidebar.text_input('Ingrese el valor a predecir :')
+
     data = st.file_uploader("Seleccione el Archivo", type=["csv", "xls", "xlsx", "json"])
     if data is not None:
         spli = os.path.splitext(data.name)
@@ -51,7 +51,6 @@ def main():
                         cx = np.array(df[cox]).reshape(-1,1)
                         cy = df[coy]
                         regr_Linear.fit(cx, cy)
-                        R = regr_Linear.score(cx,cy)
                         b0 = regr_Linear.intercept_
                         b1 = regr_Linear.coef_
                         if (b0 < 0):
@@ -62,19 +61,74 @@ def main():
                         plt.plot(cx, y_p, color='red')
                     if (model == 'Regresion Polinomial'):
                         if nivel is not None: 
-                            cx = np.array(df[cox]).reshape(-1, 1)
+                            cx = np.asarray(df[cox]).reshape(-1, 1)
                             cy = df[coy]
-                            X_train_p, X_test_p, Y_train_p, Y_test_p = train_test_split(cx, cy, test_size=0.15)
                             polgra = PolynomialFeatures(degree= int(nivel))
-                            X_train_poli = polgra.fit_transform(X_train_p)
-                            X_test_poli = polgra.fit_transform(X_test_p)
-                            regr_Linear.fit(X_train_poli, Y_train_p)
-                            Y_pred_p = regr_Linear.predict(X_test_poli)
-                            plt.plot(X_test_p, Y_pred_p, color='red')
+                            x_t = polgra.fit_transform(cx)
+                            regr_Linear.fit(x_t, cy)
+                            #plt.plot(x_t, cy)
+                            y_pred = regr_Linear.predict(x_t)
+                            plt.plot(cx, y_pred, color='red')
                     plt.savefig('Dispersion.png')
                     plt.close()
                     image = Image.open('Dispersion.png')
                     st.image(image, caption="Grafica de Dispersion")
+                elif ((model == 'Regresion Lineal') or (model == 'Regresion Polinomial')) and (ne == 'Definir función de tendencia'):
+                    if (model == 'Regresion Lineal'):
+                        cx = np.array(df[cox]).reshape(-1,1)
+                        cy = df[coy]
+                        regr_Linear.fit(cx, cy)
+                        R = regr_Linear.score(cx,cy)
+                        st.write('Pendiente : ')
+                        b1 = regr_Linear.coef_
+                        st.write(b1)
+                        st.write('Intercepto : ')
+                        b0 = regr_Linear.intercept_
+                        st.write(b0)
+                        st.write('Funcion de tendencia central')
+                        if (b0 < 0):
+                            st.write(str(b1[0]) + 'x ' + str(b0))
+                        elif (b0 > 0):
+                            st.write(str(b1[0]) + 'x + ' + str(b0))
+                        st.write('Coeficiente de Correlacion')
+                        st.write(R)
+                    elif (model == 'Regresion Polinomial'):
+                        if nivel is not None: 
+                            cx = np.asarray(df[cox]).reshape(-1, 1)
+                            cy = df[coy]
+                            polgra = PolynomialFeatures(degree= int(nivel))
+                            x_t = polgra.fit_transform(cx)
+                            regr_Linear.fit(x_t, cy)
+                            st.write('Valor de los Coeficientes :')
+                            coer = regr_Linear.coef_
+                            st.write(coer)
+                            st.write('Valor del Intercepto :')
+                            st.write(regr_Linear.intercept_)
+                            st.write('Coeficiente de Correlacion :')
+                            y_pred = regr_Linear.predict(x_t)
+                            st.write(regr_Linear.score(x_t, cy))
+                            st.write('Funcion de Tendencia Central :')
+                            concatenacion = ""
+                            da = len(coer) - 1
+                            while da>=0:
+                                if da != 0:
+                                    concatenacion = concatenacion + str(round(coer[da], 0)) + 'X^' + str(da) + '+'
+                                else:
+                                    concatenacion = concatenacion + str(round(coer[da], 0))
+                                da = da - 1
+                            st.write(concatenacion)
+                elif ((model == 'Regresion Lineal') or (model == 'Regresion Polinomial')) and (ne == 'Realizar predicción de la tendencia'):
+                    if (model == 'Regresion Lineal'):
+                        cx = np.array(df[cox]).reshape(-1,1)
+                        cy = df[coy]
+                        pred1 = int(pred)
+                        regr_Linear.fit(cx, cy)
+                        b0 = regr_Linear.intercept_
+                        b1 = regr_Linear.coef_
+                        y_p = b1[0]*pred1+b0
+                        st.write('El valor que se predijo es :')
+                        st.write(y_p)
+                    
         elif spli[1] == '.xls':
             df = pd.read_excel(data)
             x  = df.head()
